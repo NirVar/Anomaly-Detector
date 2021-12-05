c106876a86f04d7aee04bdc3b626cfa16c09a33c
NirVar <nvarsanno@gmail.com>
2021-12-05 23:59:21 +0200

    Starting from the bottom

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -1,10 +1,29 @@
 package test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 
 public class TimeSeries {
-	
+
+	HashMap <String,ArrayList<Float>> table = new HashMap<>();
 	
 	public TimeSeries(String csvFileName) {
+		Scanner scanner;
+		try {
+			File file = new File(csvFileName);
+			scanner = new Scanner(file);
+			while (scanner.hasNextLine()){
+				String data = scanner.nextLine();
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
-	
 }
